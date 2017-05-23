ccdd94e845b2ce2135b546f2763a59c4e3775fff
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2017-05-23 07:35:06 -0400

    Added mdns to nss-switch

--- centos-7-jdk/scripts/install-avahi.sh
+++ centos-7-jdk/scripts/install-avahi.sh
@@ -3,3 +3,7 @@
 yum install -y avahi
 
 systemctl enable avahi-daemon
+
+yum install -y epel-release
+yum install -y nss-mdns
+sed -i 's/hosts:      files dns myhostname/hosts:          files mdns4_minimal [NOTFOUND=return] dns/g' /etc/nsswitch.conf
